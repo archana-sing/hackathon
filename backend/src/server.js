ebb65737c23dc2c050f9e8ae69ac6bec7411028c
priyanka-890 <priyankaraghav890@gmail.com>
2021-04-25 12:16:19 +0530

    socket merge changes

--- backend/src/server.js
+++ backend/src/server.js
@@ -19,21 +19,6 @@ const io = require("socket.io")(server, {
 		methods: [ "GET", "POST" ]
 	}
 })
-io.on("connection", (socket) => {
-	socket.emit("me", socket.id)
-
-	socket.on("disconnect", () => {
-		socket.broadcast.emit("callEnded")
-	})
-
-	socket.on("callUser", (data) => {
-		io.to(data.userToCall).emit("callUser", { signal: data.signalData, from: data.from, name: data.name })
-	})
-
-	socket.on("answerCall", (data) => {
-		io.to(data.to).emit("callAccepted", data.signal)
-	})
-})
 
 //const cors = require("cors")
 
@@ -77,6 +62,9 @@ io.on("connection" , socket => {
                 room : user.room,
                 users : getRoomUsers(user.room)
             })
+
+            //video chat 
+            socket.broadcast.emit("callEnded")
         })
 
     })
@@ -85,6 +73,20 @@ io.on("connection" , socket => {
     socket.on("mouse", (data) => {
         socket.broadcast.emit("mouse", data)
     })
+
+
+    //video chat applicatio 
+    socket.emit("me", socket.id)
+
+
+
+	socket.on("callUser", (data) => {
+		io.to(data.userToCall).emit("callUser", { signal: data.signalData, from: data.from, name: data.name })
+	})
+
+	socket.on("answerCall", (data) => {
+		io.to(data.to).emit("callAccepted", data.signal)
+	})
 })
 
 
